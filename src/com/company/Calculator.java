3ee1b54d8d5f63858ee9e6d4f8ba28cc2e72e0c7
Pavel Kalachev <pavelkalachev@gmail.com>
2020-05-11 15:54:26 +0300

    Initial commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Calculator {
+
+    public Floor floor;
+    public Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet){
+
+        this.floor = floor;
+        this.carpet = carpet;
+
+    }
+    public double getTotalCost(){
+       return carpet.getCost()*floor.getArea();
+    }
+
+}
